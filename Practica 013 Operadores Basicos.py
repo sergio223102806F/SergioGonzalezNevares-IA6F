4beb840357336b51016cce6d1192ec124136894c
sergio223102806F <a22310280@ceti.mx>
2025-03-06 21:26:32 -0600

    002_Sergio
    Corrección titulo practica 13 y 14

---
+++ Practica 013 Operadores Basicos.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  5 19:02:56 2025
+
+@author: elvin
+"""
+
+print(2+2)       #Suma de enteros
+
+#Operadores Basicos
+
+print(2*3)      #Multiplicacion
+print(2**2.3)   #Flotante
+
+#Division
+
+print(6 / 3)    #Division Enteros
+print(6 / 3.)   #Division Flotantes
+
+#Divisor Integrador
+print(6 // 3)
+print(6 // 3.)
+
+#Division Negativa
+print(-6 / 2.23) #Division Negativa 
+print(-5 // 2)   #Division Negativa Redondeada
+
+#Modulo
+
+print(13 % 2)    #Modulo que es lo que queda
+
+#Adision de numeros
+print(-4 + 4)    #Numeros Negativos
+print(-4. + 8)   #Adision Numeros Negativos
+
+
+
+
+
+
+
---
+++ Practica 014 Operadores y Herramientas.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  5 19:26:26 2025
+
+@author: elvin
+"""
+
+print(9 % 6 % 2)                               #Modulo
+
+print(2 ** 2 ** 3)                             #Multiplicacion
+
+#Exponienciacion enlace derecho
+
+print(-3 ** 2)                                 #Imprime resultado                   
+
+print(-2 ** 3)                                 #Imprime resultado  
+
+print(-(3 ** 2))                               #Imprime rsultado de operacion
+
+#Multiplicacion y Residuo
+
+print(2 * 3 % 5)                               #Muestra resultado de la multiplicacion
+
+#Operadores con [arentesis]
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2) #Los numeros en los parentesis se calculan primero
+
