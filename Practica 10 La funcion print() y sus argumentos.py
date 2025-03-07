ec7912fc6fb9a7761f6a909843c4cdc8619b27e1
sergio223102806F <a22310280@ceti.mx>
2025-03-06 20:50:31 -0600

    Add files via upload

---
+++ Practica 10 La funcion print() y sus argumentos.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 15:53:38 2025
+
+@author: elvin
+"""
+
+print("Programando", "ensenciales", "en") #Imprime dialogo
+print("Python... SEEEEEE XD")             #Imprime segunda parte del dialogo
---
+++ Practica 9 Argumentos Clave.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 15:37:02 2025
+
+@author: elvin
+"""
+
+print("Me llamo", "Bond.", end="")                              #Muestra primera linea de Argumento clave
+print("James Bond.")                                            #Muestra segunda linea de Argumeto Clave
+
+print("Mi", "nombre", "es", "Bond", "James Bond", sep="-")      #Muestra linea de dialogo con guion
+print("Mi", "nombre", "es", sep="_", end="*")                   #Muetra linea de dialogo con caracter
+print("Bond", "James Bond", sep="*", end="\n")                  #Muestra linea dedialogo con *
