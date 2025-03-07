6c729a545986081766140aaf3dacf6026ef99386
sergio223102806F <a22310280@ceti.mx>
2025-03-06 21:20:35 -0600

    000_Sergio
    Modificación del titulo de prácticas

---
+++ Practica 012 Enteros, flotantes, Booleans y caracteres.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 16:16:19 2025
+
+@author: elvin
+"""
+
+print("2")                                              #Imprime Entero
+print(2)                                                #Manera alternativa de imprimir entero
+
+print(0o123)                                            #Imprime Numero Binario
+
+print(10E-2)                                            #Imprime caracter flotante
+
+print('Me gustan "Los tacos"')                          #Imprime dialogo
+
+print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")  #Imprime un dialogo centrado
+
--- Practica 10 La funcion print() y sus argumentos.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 28 15:53:38 2025
-
-@author: elvin
-"""
-
-print("Programando", "ensenciales", "en") #Imprime dialogo
-print("Python... SEEEEEE XD")             #Imprime segunda parte del dialogo
