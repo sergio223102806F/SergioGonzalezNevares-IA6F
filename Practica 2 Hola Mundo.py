53d83a709e170da8a0f78025a28f9bc730148242
sergio223102806F <a22310280@ceti.mx>
2025-03-06 20:46:06 -0600

    Add files via upload

---
+++ Practica 2 Hola Mundo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 15:08:39 2025
+
+@author: elvin
+"""
+
+print("HELLOOO WORLD.....") #Imprimir las palabras
+print("Hola Mundo")         #Nueva Palabra para mostrar
