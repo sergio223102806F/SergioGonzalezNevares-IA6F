7364a8017d51b84152aef821e1cbb69889795d85
sergio223102806F <a22310280@ceti.mx>
2025-03-06 20:49:18 -0600

    Add files via upload

---
+++ Practica 5 Instrucciones.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 15:19:12 2025
+
+@author: elvin
+"""
+    #Mostrar instruccion
+print("Monica, necesito que dejes de fumar") 
+    #Mostrar Respuesta
+print("Y yo necesito que dejes de ver a esa tipa, Raul")
---
+++ Practica 8 Argumentos Posicionales.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 15:29:41 2025
+
+@author: elvin
+"""
+
+print("Me llamo", "Bond") #Muestra la primera linea de dialogo
+print("James Bond")       #Muestra la segunda linea de dialogo
