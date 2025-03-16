1b5b1bb233078aa1bcbdb9e2d8a4a50fe81495f5
sergio223102806F <a22310280@ceti.mx>
2025-03-16 12:11:11 -0600

    Nueva version practica 7
    Nueva version practica 7

--- Practica 007 Multiples Argumentos.py
+++ Practica 007 Multiples Argumentos.py
@@ -6,3 +6,34 @@ Created on Fri Feb 28 15:26:54 2025
 """
     #Manera alternativa para acomodar los multiples argumentos a la practica 6
 print("The itsy bitsy spider, climbed up the waterspout, down come the rain, and washed the spider out")
+
+
+# Imprime la primera estrofa de la canción
+print("The itsy bitsy spider climbed up the waterspout.")  # La araña sube por el canalón.
+print("Down came the rain and washed the spider out.")     # La lluvia lava a la araña.
+print("Out came the sun and dried up all the rain,")       # El sol seca la lluvia.
+print("And the itsy bitsy spider climbed up the spout again.\n")  # La araña vuelve a subir.
+
+# Imprime la segunda estrofa de la canción
+print("The itsy bitsy spider climbed up the kitchen wall.")# La araña sube por la pared de la cocina.
+print("Swoosh! Went the fan and made the spider fall.")    # Un ventilador hace caer a la araña.
+print("Out came the sun and dried up all the rain,")       # El sol seca la lluvia.
+print("And the itsy bitsy spider climbed up the wall again.\n")  # La araña vuelve a subir.
+
+# Imprime la tercera estrofa de la canción
+print("The itsy bitsy spider climbed up the yellow pail.") # La araña sube por un cubo amarillo.
+print("In came a mouse and flicked her with his tail.")    # Un ratón golpea a la araña con su cola.
+print("Out came the sun and dried up all the rain,")       # El sol seca la lluvia.
+print("And the itsy bitsy spider climbed up the pail again.\n")  # La araña vuelve a subir.
+
+# Imprime la cuarta estrofa de la canción
+print("The itsy bitsy spider climbed up the rocking chair.")# La araña sube por una mecedora.
+print("Up jumped a cat and knocked her in the air.")        # Un gato lanza a la araña al aire.
+print("Out came the sun and dried up all the rain,")        # El sol seca la lluvia.
+print("And the itsy bitsy spider climbed up the chair again.\n")  # La araña vuelve a subir.
+
+# Imprime la quinta estrofa de la canción
+print("The itsy bitsy spider climbed up the maple tree.")   # La araña sube por un árbol de arce.
+print("She slipped on some bark and landed next to me.")    # La araña resbala y cae cerca de mí.
+print("Out came the sun and dried up all the rain,")        # El sol seca la lluvia.
+print("And the itsy bitsy spider climbed up the tree again.\n")# La araña vuelve a subir.
