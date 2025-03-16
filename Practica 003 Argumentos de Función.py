94fb87b214dd2391fbee4887611af6ae265928ed
sergio223102806F <a22310280@ceti.mx>
2025-03-16 11:50:25 -0600

    Nueva version Practica 3
    Version actualizada practica 3

--- Practica 003 Argumentos de Función.py
+++ Practica 003 Argumentos de Función.py
@@ -5,35 +5,50 @@ Created on Fri Feb 28 15:11:13 2025
 @author: elvin
 """
 
-print("HELLOOO WORLD.....") #Imprimir Hello World
-print("Hola Mundo")         #Palabras mostrada en practica 2
-print("Hola, Sergio")       #Nueva frase a mostrar
+print("HELLOOO WORLD.....")                          #Imprimir Hello World
+print("Hola Mundo")                                  #Palabras mostrada en practica 2
+print("Hola, Sergio")                                #Nueva frase a mostrar
 
 #Version Actualizada
 # Imprime la primera frase
-print("HELLOOO WORLD.....")  # Imprime "HELLOOO WORLD....."
+print("HELLOOO WORLD.....")                          # Imprime "HELLOOO WORLD....."
 
 # Imprime la segunda frase
-print("Hola Mundo")  # Imprime "Hola Mundo"
+print("Hola Mundo")                                  # Imprime "Hola Mundo"
 
 # Imprime la tercera frase
-print("Hola, Sergio")  # Imprime "Hola, Sergio"
+print("Hola, Sergio")                                # Imprime "Hola, Sergio"
 
 # Imprime la cuarta frase
-print("Esto es un mensaje en mayúsculas")  # Imprime un mensaje en minúsculas
+print("Esto es un mensaje en mayúsculas")            # Imprime un mensaje en minúsculas
 
 # Imprime la quinta frase
-print("Repetido Repetido Repetido")  # Imprime un mensaje repetido
+print("Repetido Repetido Repetido")                  # Imprime un mensaje repetido
 
 # Imprime la sexta frase
-print("*********************")  # Imprime un borde decorativo
-print("*   Decorado     *")     # Imprime un mensaje decorado
-print("*********************")  # Imprime un borde decorativo
+print("*********************")                      # Imprime un borde decorativo
+print("*   Decorado     *")                         # Imprime un mensaje decorado
+print("*********************")                      # Imprime un borde decorativo
 
 # Imprime la séptima frase
-print("S")  # Imprime el primer carácter de "Saltos"
-print("a")  # Imprime el segundo carácter de "Saltos"
-print("l")  # Imprime el tercer carácter de "Saltos"
-print("t")  # Imprime el cuarto carácter de "Saltos"
-print("o")  # Imprime el quinto carácter de "Saltos"
-print("s")  # Imprime el sexto carácter de "Saltos"
+print("S")                                          # Imprime el primer carácter de "Saltos"
+print("a")                                          # Imprime el segundo carácter de "Saltos"
+print("l")                                          # Imprime el tercer carácter de "Saltos"
+print("t")                                          # Imprime el cuarto carácter de "Saltos"
+print("o")                                          # Imprime el quinto carácter de "Saltos"
+print("s")                                          # Imprime el sexto carácter de "Saltos"
+
+# Imprime la octava frase
+print("Bienvenido al programa")                     # Imprime un mensaje de bienvenida
+
+# Imprime la novena frase
+print("Este es un ejemplo de código simple")        # Imprime una descripción del código
+
+# Imprime la décima frase
+print("¡Python es genial!")                         # Imprime un mensaje sobre Python
+
+# Imprime la undécima frase
+print("Adiós, mundo")                               # Imprime un mensaje de despedida
+
+# Imprime la duodécima frase
+print("Fin del programa")                           # Imprime un mensaje de finalización
