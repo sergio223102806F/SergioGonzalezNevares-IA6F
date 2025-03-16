9026f7f3ed64e773a995a5b4bca3b9bdcdaf9505
sergio223102806F <a22310280@ceti.mx>
2025-03-16 12:34:04 -0600

    Upload practica 10 y 11
    version mejorada practica 10 y 11

--- Practica 010 La funcion print() y sus argumentos.py
+++ Practica 010 La funcion print() y sus argumentos.py
@@ -5,5 +5,32 @@ Created on Fri Feb 28 15:53:38 2025
 @author: elvin
 """
 
-print("Programando", "ensenciales", "en") #Imprime dialogo
-print("Python... SEEEEEE XD")             #Imprime segunda parte del dialogo
+# Imprime la primera parte del diálogo
+print("Programando", "esenciales", "en")           # Primera parte del diálogo
+
+# Imprime la segunda parte del diálogo
+print("Python... SEEEEEE XD")                      # Segunda parte del diálogo
+
+# Imprime la primera parte del diálogo con guiones como separador
+print("Programando", "esenciales", "en", sep="-")  # Diálogo con guiones entre palabras
+
+# Imprime la segunda parte del diálogo con signo de exclamación al final
+print("Python... SEEEEEE XD", end="!\n")           # Diálogo con exclamación y salto de línea
+
+# Imprime la primera parte del diálogo con espacios como separador
+print("Programando", "esenciales", "en", sep=" ")  # Diálogo con espacios entre palabras
+
+# Imprime la segunda parte del diálogo con asteriscos como separador
+print("Python", "SEEEEEE", "XD", sep="*", end="\n")# Diálogo con asteriscos y salto de línea
+
+# Imprime la primera parte del diálogo con comas como separador
+print("Programando", "esenciales", "en", sep=", ") # Diálogo con comas entre palabras
+
+# Imprime la segunda parte del diálogo con signo de interrogación al final
+print("Python... SEEEEEE XD", end="?\n")           # Diálogo con interrogación y salto de línea
+
+# Imprime la primera parte del diálogo con dos puntos como separador
+print("Programando", "esenciales", "en", sep=":")  # Diálogo con dos puntos entre palabras
+
+# Imprime la segunda parte del diálogo con guiones bajos como separador
+print("Python", "SEEEEEE", "XD", sep="_", end="\n")# Diálogo con guiones bajos y salto de línea
--- Practica 011 La funcion print() y figuritas.py
+++ Practica 011 La funcion print() y figuritas.py
@@ -4,12 +4,41 @@ Created on Fri Feb 28 16:10:43 2025
 
 @author: elvin
 """
-        #Imprime una figurita mostrada dentro del conjunto de funciones 
-print("    *")
-print("   * *")
-print("  *   *")
-print(" *     *")
-print("***   ***")
-print("  *   *")
-print("  *   *")
-print("  *****")
+#Imprime una figurita mostrada dentro del conjunto de funciones 
+#Version Mejorada
+# Imprime una figurita de un corazón
+print("   ***     ***   ")  # Parte superior del corazón
+print(" *     * *     * ")  # Línea superior del corazón
+print("*       *       *")  # Centro del corazón
+print(" *     * *     * ")  # Línea inferior del corazón
+print("   ***     ***   ")  # Parte inferior del corazón
+
+# Imprime una figurita de una casa
+print("    /\    ")         # Techo de la casa
+print("   /  \   ")         # Parte superior del techo
+print("  /    \  ")         # Parte media del techo
+print(" /______\ ")         # Base del techo
+print(" |      | ")         # Pared de la casa
+print(" |  []  | ")         # Ventana de la casa
+print(" |______| ")         # Base de la casa
+
+# Imprime una figurita de un árbol
+print("    *    ")          # Copa del árbol
+print("   ***   ")          # Parte superior de la copa
+print("  *****  ")          # Parte media de la copa
+print(" ******* ")          # Parte inferior de la copa
+print("   |||   ")          # Tronco del árbol
+
+# Imprime una figurita de un gato
+print("  /\_/\  ")          # Cabeza del gato
+print(" ( o.o ) ")          # Ojos y nariz del gato
+print("  > ^ <  ")          # Orejas y boca del gato
+
+# Imprime una figurita de un cohete
+print("    /\    ")         # Punta del cohete
+print("   /  \   ")         # Parte superior del cohete
+print("  /    \  ")         # Parte media del cohete
+print(" /______\ ")         # Base del cohete
+print(" |      | ")         # Cuerpo del cohete
+print(" |  ||  | ")         # Ventanas del cohete
+print(" |______| ")         # Base del cohete
