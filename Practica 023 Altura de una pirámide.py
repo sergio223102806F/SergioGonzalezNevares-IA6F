95fec9a6ae7b07c4f4483f0970c98ab530a177c9
sergio223102806F <a22310280@ceti.mx>
2025-03-16 14:20:34 -0600

    Upgrade practica 23
    Nueva version incluida practica 23

--- Practica 023 Altura de una pirámide.py
+++ Practica 023 Altura de una pirámide.py
@@ -19,3 +19,27 @@ while bloques >= bloques_capa:
 
 # Imprimir el resultado
 print("La altura de la pirámide:", altura)
+
+#Version 2
+# Solicitar al usuario que ingrese el número de bloques y convertirlo a entero
+bloques = int(input("Ingresa el número de bloques: "))
+
+# Inicializar la altura de la pirámide en 0
+altura = 0
+
+# Inicializar el número de bloques necesarios para la primera capa
+bloques_capa = 1
+
+# Calcular la altura de la pirámide
+while bloques >= bloques_capa:
+    # Restar los bloques utilizados en la capa actual del total de bloques
+    bloques -= bloques_capa
+    
+    # Incrementar la altura de la pirámide en 1
+    altura += 1
+    
+    # Aumentar el número de bloques necesarios para la siguiente capa
+    bloques_capa += 1
+
+# Imprimir la altura final de la pirámide
+print("La altura de la pirámide:", altura)
