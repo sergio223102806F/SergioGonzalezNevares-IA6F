cad560e7620ccaa1ef45b26c6c9d9830e2980221
sergio223102806F <a22310280@ceti.mx>
2025-03-16 14:32:45 -0600

    Update practica 24
    Nueva version de la practica

--- Practica 024 Imprimir Listas.py
+++ Practica 024 Imprimir Listas.py
@@ -23,3 +23,21 @@ print("Contenido anterior de la lista:", numeros)    # Mostrar el contenido ante
 
 # Imprimir la longitud de la lista
 print("\nLongitud de la lista:", len(numeros))       # Mostrar la cantidad de elementos en la lista.
+
+#Version 2
+# Definir una lista de números
+numeros = [10, 5, 7, 2, 1]                           # Crear una lista con valores iniciales
+
+# Imprimir el contenido original de la lista
+print("Contenido original de la lista:", numeros)    # Mostrar el contenido original de la lista
+
+# Cambiar el valor del primer elemento de la lista (índice 0) a 111
+numeros[0] = 111                                     # Modificar el primer elemento de la lista
+print("\nContenido anterior de la lista:", numeros)  # Mostrar el contenido actualizado de la lista
+
+# Copiar el valor del quinto elemento (índice 4) al segundo elemento (índice 1)
+numeros[1] = numeros[4]                              # Copiar el valor del quinto elemento al segundo
+print("Contenido anterior de la lista:", numeros)    # Mostrar el contenido actualizado de la lista
+
+# Imprimir la longitud de la lista
+print("\nLongitud de la lista:", len(numeros))       # Mostrar la cantidad de elementos en la lista
