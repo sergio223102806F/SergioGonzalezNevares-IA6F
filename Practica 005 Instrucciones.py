517f7a450c8db9e285b12b6d484e0ee79aa9061c
sergio223102806F <a22310280@ceti.mx>
2025-03-16 12:06:26 -0600

    Version 2 practica 5
    Nueva version practica 5

--- Practica 005 Instrucciones.py
+++ Practica 005 Instrucciones.py
@@ -4,7 +4,50 @@ Created on Fri Feb 28 15:19:12 2025
 
 @author: elvin
 """
-    #Mostrar instruccion
-print("Monica, necesito que dejes de fumar") 
-    #Mostrar Respuesta
-print("Y yo necesito que dejes de ver a esa tipa, Raul")
+# Mostrar instrucción                                             
+print("Monica, necesito que dejes de fumar")                      # Instrucción para Mónica
+
+# Mostrar respuesta                                              
+print("Y yo necesito que dejes de ver a esa tipa, Raul")          # Respuesta de Mónica
+
+# Mostrar instrucción                                             
+print("Raul, necesito que dejes de llegar tarde")                 # Instrucción para Raúl
+
+# Mostrar respuesta                                              
+print("Y yo necesito que dejes de revisar mi teléfono, Mónica")   # Respuesta de Raúl
+
+# Mostrar instrucción                                             
+print("Mónica, necesito que dejes de gastar tanto dinero")        # Instrucción para Mónica
+
+# Mostrar respuesta                                              
+print("Y yo necesito que dejes de ser tan tacaño, Raul")          # Respuesta de Mónica
+
+# Mostrar instrucción                                             
+print("Raul, necesito que dejes de roncar")                       # Instrucción para Raúl
+
+# Mostrar respuesta                                              
+print("Y yo necesito que dejes de moverte al dormir, Mónica")     # Respuesta de Raúl
+
+# Mostrar instrucción                                             
+print("Mónica, necesito que dejes de discutir por todo")          # Instrucción para Mónica
+
+# Mostrar respuesta                                              
+print("Y yo necesito que dejes de ser tan terco, Raul")           # Respuesta de Mónica
+
+# Mostrar instrucción                                             
+print("Raul, necesito que dejes de olvidar nuestras fechas")      # Instrucción para Raúl
+
+# Mostrar respuesta                                              
+print("Y yo necesito que dejes de ser tan dramática, Mónica")     # Respuesta de Raúl
+
+# Mostrar instrucción                                             
+print("Mónica, necesito que dejes de criticar a mis amigos")      # Instrucción para Mónica
+
+# Mostrar respuesta                                              
+print("Y yo necesito que dejes de salir tanto con ellos, Raul")   # Respuesta de Mónica
+
+# Mostrar instrucción                                             
+print("Raul, necesito que dejes de dejar la ropa tirada")         # Instrucción para Raúl
+
+# Mostrar respuesta                                              
+print("Y yo necesito que dejes de ser tan perfeccionista, Mónica")# Respuesta de Raúl    
