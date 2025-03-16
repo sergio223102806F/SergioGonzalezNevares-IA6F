f1cc3fbf85a8b7d2d2355fdce86aa089ee7c2adc
sergio223102806F <a22310280@ceti.mx>
2025-03-16 14:36:20 -0600

    Update Practica 25
    Version mejorada practica 25

--- Practica 025 Remover elementos de una lista.py
+++ Practica 025 Remover elementos de una lista.py
@@ -6,29 +6,25 @@ Created on Sat Mar 15 15:58:01 2025
 """
 
 # Definir una lista de números
-numeros = [10, 5, 7, 2, 1]
+numeros = [10, 5, 7, 2, 1]                        # Crear una lista con valores iniciales
 
 # Imprimir el contenido original de la lista
-print("Contenido original de la lista:", numeros)  # Mostrar el contenido original de la lista.
+print("Contenido original de la lista:", numeros)  # Mostrar el contenido original de la lista
 
 # Cambiar el valor del primer elemento de la lista (índice 0) a 111
-numeros[0] = 111
-# Imprimir el contenido actualizado de la lista
-print("\nContenido anterior de la lista:", numeros)  # Mostrar el contenido anterior de la lista.
+numeros[0] = 111                                   # Modificar el primer elemento de la lista
+print("\nContenido anterior de la lista:", numeros) # Mostrar el contenido actualizado de la lista
 
 # Copiar el valor del quinto elemento (índice 4) al segundo elemento (índice 1)
-numeros[1] = numeros[4]
-# Imprimir el contenido actualizado de la lista
-print("Contenido anterior de la lista:", numeros)  # Mostrar el contenido anterior de la lista.
+numeros[1] = numeros[4]                            # Copiar el valor del quinto elemento al segundo
+print("Contenido anterior de la lista:", numeros)  # Mostrar el contenido actualizado de la lista
 
 # Imprimir la longitud de la lista
-print("\nLongitud de la lista:", len(numeros))  # Mostrar la cantidad de elementos en la lista.
+print("\nLongitud de la lista:", len(numeros))      # Mostrar la cantidad de elementos en la lista
 
 ###
 
 # Eliminar el segundo elemento de la lista (índice 1)
-del numeros[1]
-# Imprimir la nueva longitud de la lista
-print("Longitud de la nueva lista:", len(numeros))  # Mostrar la nueva cantidad de elementos en la lista.
-# Imprimir el contenido actualizado de la lista
-print("\nContenido de la nueva lista:", numeros)  # Mostrar el contenido actual de la lista.
+del numeros[1]                                     # Eliminar el segundo elemento de la lista
+print("Longitud de la nueva lista:", len(numeros))  # Mostrar la nueva cantidad de elementos en la lista
+print("\nContenido de la nueva lista:", numeros)    # Mostrar el contenido actual de la lista
