e773621cb1bbf72bce04e25c5d146a0723134580
sergio223102806F <a22310280@ceti.mx>
2025-03-16 11:57:57 -0600

    Nueva Version Practica 4
    Version 2 practica 4

--- Practica 004 Invocacion de Función.py
+++ Practica 004 Invocacion de Función.py
@@ -9,3 +9,40 @@ print("HELLOOO WORLD.....") #Mostrar frase practica 1
 print("Hola Mundo")         #Frase practica 2
 print("Hola, Sergio")       #Frase Practica 3
 print("Hola, Sergio?")      #Frase nueva practica 4
+
+#Version 2
+# Imprime la primera frase
+print("HELLOOO WORLD.....") # Mostrar frase práctica 1
+
+# Imprime la segunda frase
+print("Hola Mundo")         # Frase práctica 2
+
+# Imprime la tercera frase
+print("Hola, Sergio")       # Frase práctica 3
+
+# Imprime la cuarta frase
+print("Hola, Sergio?")      # Imprime frase nueva
+
+# Imprime la quinta frase
+print("¿Cómo estás, Sergio?")# Imprime frase nueva
+
+# Imprime la sexta frase
+print("Sergio, bienvenido ")# Imprime frase nueva
+
+# Imprime la séptima frase
+print("Sergio,¿qué tal?")   # Imprime frase nueva
+
+# Imprime la octava frase
+print("Sergio, eres increíble")#Imprime frase nueva
+
+# Imprime la novena frase
+print("Sergio, ¡vamos a programar!")# Imprime frase nueva
+
+# Imprime la décima frase
+print("Sergio, ¿qué planes para hoy?")# Imprime frase nueva
+
+# Imprime la undécima frase
+print("Sergio, siempre aprendiendo")# Imprime Frase nueva 
+
+# Imprime la duodécima frase
+print("Sergio, ¡hasta la próxima, MAI!")# Imprime Frase nueva
